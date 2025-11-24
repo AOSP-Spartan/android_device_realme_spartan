3ae29981a6e8f1a45d8cb011af7bc47d069c6184
bijoyv9 <bijoyv9@gmail.com>
2025-11-24 05:02:47 +0000

    spartan: Adapt to PixelOS
    Change-Id: I674a00ff1eacdc928ed3b78f4195417a8ebfd9cb

--- custom_spartan.mk
+++ custom_spartan.mk
@@ -12,14 +12,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spartan device
 $(call inherit-product, device/realme/spartan/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_spartan
+PRODUCT_NAME := custom_spartan
 PRODUCT_DEVICE := spartan
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3371
+
+PRODUCT_NO_CAMERA := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
